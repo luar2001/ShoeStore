e4564b35b4488fbccf8255a1f652221e8c660b68
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-02-21 18:57:27 +0100

    Created the Category object

---
+++ src/Category.java
@@ -0,0 +1,55 @@
+/*
+ * Created by Lukas Aronsson
+ * Date: 20/02/2021
+ * Time: 17:34
+ * Project: ShoeStore
+ * Copyright: MIT
+ */
+
+/**
+ * Category represent a category in the Categories table in the database ShoeStore
+ * <p>
+ * A Category has
+ * id: id of the Category in the Categories table
+ * category: category of the Category
+ */
+public class Category {
+    private int id = 0; // id: id of the Category in the Categories table
+    private String category = null; // category: category of the Category
+
+    /**
+     * Getter for the id of the Category
+     *
+     * @return id: id of the Category in the Categories table
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Setter for the id of the Category
+     *
+     * @param id id: id of the Category in the Categories table
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter for the category of the Category
+     *
+     * @return category: category of the Category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Setter for the category of the Category
+     *
+     * @param category category: category of the Category
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
